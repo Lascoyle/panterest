055e3844bcc46bba4eecc735460fb8af408cfb1f
Lascoyle <meursault45@live.com>
2020-12-03 12:13:39 +0100

    Make profile informations editable

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccountController extends AbstractController
 {
     /**
-     * @Route("/account", name="app_account")
+     * @Route("/account", name="app_account", methods="GET")
      */
     public function show(): Response
     {
         return $this->render('account/show.html.twig');
+    }
+
+    /**
+     * @Route("/account/{id<[0-9]+>}/edit", name="app_account_edit", methods="GET|PUT")
+     */
+    public function edit(Request $request, EntityManagerInterface $em) :Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserFormType::class, $user, [
+            'method' => 'PUT'
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Your account informations was successfully updated!');
+
+            return $this->redirectToRoute('app_account');
+        }
+        return $this->render('account/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
